80755d52171c5a8bbc33d49b8021fd00bd76f627
peterN12g <peternguyensmhs@gmail.com>
2024-07-15 12:36:14 -0500

    update resume and project design

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -11,28 +11,31 @@
         <div class='article-container'>
             <article>
                 <header>AniFinder</header>
+                <div>
+                <img _ngcontent-ng-c3205807728="" class="skill-image" alt="HTML,html5" src="https://img.shields.io/badge/HTML-dee0e3?logo=html5" style="width: 40px; height: 20px;">
+                <img _ngcontent-ng-c3205807728="" class="skill-image" alt="CSS,css3&amp;logoColor=blue" src="https://img.shields.io/badge/CSS-dee0e3?logo=css3&amp;logoColor=blue" style="width: 40px; height: 20px;">
+                <img _ngcontent-ng-c3205807728="" class="skill-image" alt="JavaScript,javascript" src="https://img.shields.io/badge/JavaScript-dee0e3?logo=javascript" style="width: 60px; height: 20px;">
+                <img alt="Static Badge" src="https://img.shields.io/badge/Svelte-orange?logo=Svelte&logoColor=orange">
+            </div>
                 <p>
                     AniFinder is an AI powered Anime generator based on the user's preferences. The generator is powered by  google's AI, gemini pro.
                     The prompts include an anime genre and reference anime to generate a similar anime the user may enjoy.
                 </p>
                 <footer>
-                    <a href="https://github.com/peterN12g/anime" target="_blank">GitHub</a>
-                    <a href="https://anifinder.pages.dev/" target="_blank">Hosted Site</a>
-                </footer>
-            </article>
-            <article>
-                <header>Warmup Calculator</header>
-                <p>
-                    A simple weightlifting warmup calculator to help lifters work up to their personal best attempt safely.
-                    The calculator works seamlessly on mobile as a Progressive Web App (PWA) for user convenience.
-                </p>
-                <footer>
-                    <a href="https://github.com/peterN12g/warmupCalc" target="_blank">GitHub</a>
-                    <a href="https://warmup.pages.dev/" target="_blank">Hosted Site</a>
+                    <a href="https://github.com/peterN12g/anime" class="github" target="_blank"><i class="bi bi-github" style="font-size: 30px;"></i></a>
+                    <a href="https://anifinder.pages.dev/" target="_blank"><i class="bi bi-browser-chrome" style="font-size: 30px;"></i></a>
                 </footer>
             </article>
             <article>
                 <header>GymShark Scraper</header>
+                <div>
+                <img _ngcontent-ng-c3205807728="" class="skill-image" alt="HTML,html5" src="https://img.shields.io/badge/HTML-dee0e3?logo=html5" style="width: 40px; height: 20px;">
+                <img _ngcontent-ng-c3205807728="" class="skill-image" alt="CSS,css3&amp;logoColor=blue" src="https://img.shields.io/badge/CSS-dee0e3?logo=css3&amp;logoColor=blue" style="width: 40px; height: 20px;">
+                <img _ngcontent-ng-c3205807728="" class="skill-image" alt="JavaScript,javascript" src="https://img.shields.io/badge/JavaScript-dee0e3?logo=javascript" style="width: 60px; height: 20px;">
+                <img alt="Static Badge" src="https://img.shields.io/badge/Python-white?logo=Python&logoColor=blue%2Cyellow">
+                <img alt="Static Badge" src="https://img.shields.io/badge/React-lightgray?logo=React">
+                <img alt="Static Badge" src="https://img.shields.io/badge/Flask-lightgray?logo=Flask&logoColor=aqua">
+            </div>
                 <p>
                     a Python-based web scraper focused on GymShark products. The server is hosted using Python Flask,
                     with Caddy used to reverse proxy over HTTPS. The backend Python server provides data via an API endpoint.
@@ -40,19 +43,41 @@
                     (PWA) that sends browser notifications through a service worker.  
                 </p>
                 <footer>
-                    <a href="https://github.com/peterN12g/GymShark-Scraper" target="_blank">GitHub</a>
-                    <a href="https://gymshark-scraper.peternguyen.tech/" target="_blank">Hosted Site</a>
-                    <a href="https://ibb.co/17tppmR" target="_blank">Example IOS Notification</a>
+                    <a href="https://github.com/peterN12g/GymShark-Scraper" target="_blank"><i class="bi bi-github" style="font-size: 30px;"></i></a>
+                    <a href="https://gymshark-scraper.peternguyen.tech/" target="_blank"><i class="bi bi-browser-chrome" style="font-size: 30px;"></i></a>
+                </footer>
+            </article>
+            <article>
+                <header>Warmup Calculator</header>
+                <div>
+                <img _ngcontent-ng-c3205807728="" class="skill-image" alt="HTML,html5" src="https://img.shields.io/badge/HTML-dee0e3?logo=html5" style="width: 40px; height: 20px;">
+                <img _ngcontent-ng-c3205807728="" class="skill-image" alt="CSS,css3&amp;logoColor=blue" src="https://img.shields.io/badge/CSS-dee0e3?logo=css3&amp;logoColor=blue" style="width: 40px; height: 20px;">
+                <img _ngcontent-ng-c3205807728="" class="skill-image" alt="JavaScript,javascript" src="https://img.shields.io/badge/JavaScript-dee0e3?logo=javascript" style="width: 60px; height: 20px;">
+                <img alt="Static Badge" src="https://img.shields.io/badge/Svelte-orange?logo=Svelte&logoColor=orange">
+            </div>
+                <p>
+                    A simple weightlifting warmup calculator to help lifters work up to their personal best attempt safely.
+                    The calculator works seamlessly on mobile as a Progressive Web App (PWA) for user convenience.
+                </p>
+                <footer>
+                    <a href="https://github.com/peterN12g/warmupCalc" target="_blank"><i class="bi bi-github" style="font-size: 30px;"></i></a>
+                    <a href="https://warmup.pages.dev/" target="_blank"><i class="bi bi-browser-chrome" style="font-size: 30px;"></i></a>
                 </footer>
             </article>
             <article>
                 <header>Nutrition Tracker</header>
+                <div>
+                    <img _ngcontent-ng-c3205807728="" class="skill-image" alt="HTML,html5" src="https://img.shields.io/badge/HTML-dee0e3?logo=html5" style="width: 40px; height: 20px;">
+                    <img _ngcontent-ng-c3205807728="" class="skill-image" alt="CSS,css3&amp;logoColor=blue" src="https://img.shields.io/badge/CSS-dee0e3?logo=css3&amp;logoColor=blue" style="width: 40px; height: 20px;">
+                    <img _ngcontent-ng-c3205807728="" class="skill-image" alt="JavaScript,javascript" src="https://img.shields.io/badge/JavaScript-dee0e3?logo=javascript" style="width: 60px; height: 20px;">
+                    <img alt="Static Badge" src="https://img.shields.io/badge/Svelte-orange?logo=Svelte&logoColor=orange">
+                </div>
                 <p>
                     The nutrition tracker provides users the ability to calculate their basal metabolic rate (BMR) and lose weight with the calorie tracker. 
                     Information about meals is API returned. Users can add specific foods, which then subtract the provided macronutrients from their daily goals. 
                 </p>
                 <footer>
-                    <a href="https://github.com/peterN12g/nutritiontrack" target="_blank">GitHub</a>
+                    <a href="https://github.com/peterN12g/nutritiontrack" target="_blank"><i class="bi bi-github" style="font-size: 30px;"></i></a>
                 </footer>
             </article>
         </div>
